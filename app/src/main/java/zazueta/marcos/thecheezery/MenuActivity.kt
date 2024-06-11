8613d0965e08551777d404097fe7308a920980d5
Marcos-Ruiz1 <Marcos.ZazuetaRuiz1@gmail.com>
2024-06-10 23:02:49 -0700

    add products views (salties, sweets, cold and hot)

--- app/src/main/java/zazueta/marcos/thecheezery/MenuActivity.kt
+++ app/src/main/java/zazueta/marcos/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package zazueta.marcos.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +14,26 @@ class MenuActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
 
+        val btnCold: Button = findViewById(R.id.button_cold_drinks)
+        val btnHot: Button = findViewById(R.id.button_hot_drinks)
+        val btnSweet: Button = findViewById(R.id.button_sweets)
+        val btnSalties: Button = findViewById(R.id.button_salties)
 
+        btnCold.setOnClickListener {
+            val intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
+        btnHot.setOnClickListener {
+            val intent: Intent = Intent(this, HotDrinksActivity::class.java)
+            startActivity(intent)
+        }
+        btnSweet.setOnClickListener {
+            val intent: Intent = Intent(this, SweetsActivity::class.java)
+            startActivity(intent)
+        }
+        btnSalties.setOnClickListener {
+            val intent: Intent = Intent(this, SaltiesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/zazueta/marcos/thecheezery/Product.kt
@@ -0,0 +1,7 @@
+package zazueta.marcos.thecheezery
+
+data class Product (
+    var name: String,
+    var image: Int,
+    var description: String,
+    var price: Double)
